ffded6527bf0cf0b31451f81f8e1ad0ab5058073
goodCodeForGood <sahitihibane6@gmail.com>
2024-03-14 00:18:53 -0700

    Add login form page

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React, { useState } from "react";
+import LoginForm from "@/components/LoginForm"
+
+export default function Home() {
+    return <main>
+        <LoginForm />
+    </main>;
+}
---
+++ components/LoginForm.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+export default function LoginForm() {
+    return <div>Login Form</div>
+}
