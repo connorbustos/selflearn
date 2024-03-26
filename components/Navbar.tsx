6d693846db55bf7afc49f74fa5553d1b2b8a7906
connorbustos <thedestroyercb@gmail.com>
2024-03-26 13:33:13 -0700

    Add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="text-black py-3 px-6 flex items-end border-b border-gray-300">
+      <div className="text-xl font-bold mr-8 flex items-end scale-125">
+        SelfLearn
+      </div>
+      <ul className="flex space-x-4 items-end">
+        <li>
+          <Link href="/search">
+            <div className="hover:text-gray-300">Search</div>
+          </Link>
+        </li>
+        <li>
+          <Link href="/create_new_wiki">
+            <div className="hover:text-gray-300">Create</div>
+          </Link>
+        </li>
+        <li>
+          <Link href="/profile">
+            <div className="hover:text-gray-300">Profile</div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
