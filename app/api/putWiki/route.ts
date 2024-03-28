cdeae554ba60364cedab23a1c9708455a68b7b3d
connorbustos <thedestroyercb@gmail.com>
2024-03-28 16:37:44 -0700

    PutWiki API

--- app/api/putWiki/route.ts
+++ app/api/putWiki/route.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 
+import { WikiContent, WikiData } from "@/app/types/Wiki";
+
 import clientPromise from "../../../libs/db";
-
-
 
 export async function POST(request: Request) {
   const client = await clientPromise;
   const db = client.db();
-  const result = await db
-    .collection("AllWikis")
-    .insertOne({ name: "API REQUEST" });
-  return NextResponse.json({ message: "Post Request Working", result: result });
+  const wikiData = await request.json();
+  console.log(wikiData);
+  const result = await db.collection("AllWikis").insertOne(wikiData);
+  return NextResponse.json({ message: "Put Wiki Successful", result: result });
 }
