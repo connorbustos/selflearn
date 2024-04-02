79222afd57c3845d82f0bc4569ad855dab56b008
connorbustos <thedestroyercb@gmail.com>
2024-04-02 13:32:04 -0700

    Add Navbar conditional rendering, refactor navbar to remove unnecessary <li>, and add static login and signup pages to be used with ShadCN

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Navbar from "../components/Navbar";
+import { usePathname } from "next/navigation";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,10 +9,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body>
-        <Navbar />
+        <Navbar currentPath={pathname} />
         {children}
       </body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,32 +1,34 @@
 import Link from "next/link";
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+  currentPath: string;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentPath }) => {
+  const disableNavbar = ["/login", "/signup"];
+
+  if (disableNavbar.includes(currentPath)) {
+    return null;
+  }
+
   return (
     <nav className="text-black py-3 px-6 flex items-end border-b border-gray-300">
       <div className="text-xl font-bold mr-8 flex items-end scale-125">
         SelfLearn
       </div>
       <ul className="flex space-x-4 items-end">
-        <li>
-          <Link href="/search">
-            <div className="hover:text-gray-300">Search</div>
-          </Link>
-        </li>
-        <li>
-          <Link href="/create_new_wiki">
-            <div className="hover:text-gray-300">Create</div>
-          </Link>
-        </li>
-        <li>
-          <Link href="/view_all_wikis">
-            <div className="hover:text-gray-300">View All Wikis</div>
-          </Link>
-        </li>
-        <li>
-          <Link href="/profile">
-            <div className="hover:text-gray-300">Profile</div>
-          </Link>
-        </li>
+        <Link href="/search">
+          <div className="hover:text-gray-300">Search</div>
+        </Link>
+        <Link href="/create_new_wiki">
+          <div className="hover:text-gray-300">Create</div>
+        </Link>
+        <Link href="/view_all_wikis">
+          <div className="hover:text-gray-300">View All Wikis</div>
+        </Link>
+        <Link href="/profile">
+          <div className="hover:text-gray-300">Profile</div>
+        </Link>
       </ul>
     </nav>
   );
