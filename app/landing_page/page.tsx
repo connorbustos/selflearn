787d8b034d1e103a53e7d7a203fc78b3c40e95c5
connorbustos <thedestroyercb@gmail.com>
2024-04-22 21:58:37 -0700

    fix hydration error by making link stylesheet 1st div in landing page

--- app/landing_page/page.tsx
+++ app/landing_page/page.tsx
@@ -3,44 +3,47 @@ import { TypeAnimation } from "react-type-animation";
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen font-Proxima-Nova">
-      {/* main welcome section */}
-      <div className="w-full h-[80vh] flex flex-col justify-center items-center gap-y-2">
-        <p className="text-8xl font-normal">
-          Welcome to <span className="text-8xl font-black">Selflearn.</span>
-        </p>
-        <TypeAnimation
-          sequence={[
-            500,
-            "Built for Students.",
-            2000,
-            "Built for Educators.",
-            2000,
-            "Built for Enthusiasts.",
-            2000,
-            "Built for Everyone.",
-            () => {
-              console.log("Sequence completed");
-            },
-          ]}
-          speed={50}
-          wrapper="span"
-          cursor={true}
-          repeat={Infinity}
-          style={{
-            fontSize: "2em",
-            display: "inline-block",
-            fontFamily: "Proxima-Nova",
-            fontWeight: 500,
-          }}
-        />
+    <>
+      <link rel="stylesheet" href="https://use.typekit.net/urj3apl.css" />
+      <div className="flex flex-col justify-center items-center w-screen h-screen font-Proxima-Nova">
+        {/* main welcome section */}
+        <div className="w-full h-[80vh] flex flex-col justify-center items-center gap-y-2">
+          <p className="text-8xl font-normal">
+            Welcome to <span className="text-8xl font-black">SelfLearn.</span>
+          </p>
+          <TypeAnimation
+            sequence={[
+              500,
+              "Built for Students.",
+              2000,
+              "Built for Educators.",
+              2000,
+              "Built for Enthusiasts.",
+              2000,
+              "Built for Everyone.",
+              () => {
+                console.log("Sequence completed");
+              },
+            ]}
+            speed={50}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            style={{
+              fontSize: "2em",
+              display: "inline-block",
+              fontFamily: "Proxima-Nova",
+              fontWeight: 500,
+            }}
+          />
+        </div>
+
+        {/* banner */}
+        <div className="font-bold">banner</div>
+
+        {/* How it works */}
+        <div>how it works</div>
       </div>
-
-      {/* banner */}
-      <div className="font-bold">banner</div>
-
-      {/* How it works */}
-      <div>how it works</div>
-    </div>
+    </>
   );
 }
