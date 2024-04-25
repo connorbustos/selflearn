c441eae3658192091e0ba7b25c112c8a515f4fe7
goodCodeForGood <sahitihibane6@gmail.com>
2024-04-24 23:00:27 -0700

    Remove old home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <body>
           <Navbar currentPath={pathName} />
           {children}
-          <Footer></Footer>
+          <Footer/>
         </body>
       </html>
     </SessionWrapper>
