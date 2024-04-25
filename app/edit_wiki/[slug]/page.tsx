967d37fd3d05296d4f70dc987c59ac88ff1f184e
connorbustos <thedestroyercb@gmail.com>
2024-04-25 16:42:53 -0700

    remove hardcode api on edit_wiki and cleanup

--- app/edit_wiki/[slug]/page.tsx
+++ app/edit_wiki/[slug]/page.tsx
@@ -10,9 +10,7 @@ async function generateSlug(slug: string) {
   let wikiData: WikiData | null = null;
   try {
     const endpoint = isDraft ? "getWikiDraft" : "getWiki";
-    const response = await fetch(
-      `http://localhost:3000/api/${endpoint}?wikiId=${wikiId}`
-    );
+    const response = await fetch(`/api/${endpoint}?wikiId=${wikiId}`);
     if (!response.ok) {
       throw new Error(`Failed to fetch wiki data from ${endpoint}`);
     }
@@ -40,12 +38,7 @@ const EditWiki = ({ params }: { params: { slug: string } }) => {
   return (
     <ChakraProvider>
       <div className="w-full py-4 px-4 content-center">
-        {
-          wikiData ? <EditWikiLayout wiki={wikiData} /> : null
-          // <div className="absolute inset-0 flex justify-center items-center pt-80">
-          //   null
-          // </div>
-        }
+        {wikiData ? <EditWikiLayout wiki={wikiData} /> : null}
       </div>
     </ChakraProvider>
   );
