c2de71437c28f16ac0c36455e3131858424edaa3
goodCodeForGood <sahitihibane6@gmail.com>
2024-04-24 23:16:25 -0700

    Add linkedin link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,33 +1,30 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { render } from "react-dom";
 
 const Footer = () => {
-        return (
-            <div id="footer">
-                <div className="text-center py-4">
-                    <div className="contact-info">
-                    <Link href="https://www.linkedin.com/in/connorbustos">
-                        Connor Bustos
-                    </Link> 
-                    <span> | </span>
-                    <Link href="https://www.linkedin.com/in/sahiti-hibane/">
-                        Sahiti Hibane
-                    </Link>
-                    <span> | </span>
-                    <Link href="https://www.linkedin.com/in/isaac-kimmi">
-                        Isaac Kim
-                    </Link>
-                    <span> | </span>
-                    <Link href="">
-                        Nguyen Chau
-                    </Link>
-                    </div>
-                    <p>Copyright © 2024, SelfLearn LLC. All Rights Reserved.</p>
-                </div>
-            </div>
-        )    
-}
+  return (
+    <div id="footer">
+      <div className="text-center py-4">
+        <div className="contact-info">
+          <Link href="https://www.linkedin.com/in/connorbustos">
+            Connor Bustos
+          </Link>
+          <span> | </span>
+          <Link href="https://www.linkedin.com/in/sahiti-hibane/">
+            Sahiti Hibane
+          </Link>
+          <span> | </span>
+          <Link href="https://www.linkedin.com/in/isaac-kimmi">Isaac Kim</Link>
+          <span> | </span>
+          <Link href="https://www.linkedin.com/in/nguyen-chau-754b74264/">
+            Nguyen Chau
+          </Link>
+        </div>
+        <p>Copyright © 2024, SelfLearn LLC. All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
