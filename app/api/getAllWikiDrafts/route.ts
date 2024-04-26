a8cb4a0aefeb8c44ea948933c1c326b57cc1f440
connorbustos <thedestroyercb@gmail.com>
2024-04-25 18:17:59 -0700

    fix getAllWikiDrafts calling wrong collection oops

--- app/api/getAllWikiDrafts/route.ts
+++ app/api/getAllWikiDrafts/route.ts
@@ -6,11 +6,11 @@ export async function GET(request: Request) {
     const client = await clientPromise;
     const db = client.db();
     const collectionName =
-      process.env.NODE_ENV === "development" ? "AllWikis" : "AllWikisProd";
+      process.env.NODE_ENV === "development" ? "WikiDrafts" : "WikiDraftsProd";
     const url = new URL(request.url);
     const timestamp = url.searchParams.get("timestamp");
     // There was a bug on prod where if the request parameter wasn't used, data was forcefully cached.
-    console.log(`getAllWikis Request received at timestamp: ${timestamp}`);
+    console.log(`getAllWikiDrafts Request received at timestamp: ${timestamp}`);
     const result = await db.collection(collectionName).find().toArray();
     return NextResponse.json(result);
   } catch (error) {
