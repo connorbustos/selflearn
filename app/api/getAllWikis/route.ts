f0ea27dc0f2fbaba75404961aa24f249fe5a034c
connorbustos <thedestroyercb@gmail.com>
2024-04-25 17:41:05 -0700

    try hack to force request to be used

--- app/api/getAllWikis/route.ts
+++ app/api/getAllWikis/route.ts
@@ -1,12 +1,13 @@
 import { NextResponse } from "next/server";
 import clientPromise from "../../../libs/db";
 
-export async function GET() {
+export async function GET(request: Request) {
   try {
     const client = await clientPromise;
     const db = client.db();
     const collectionName =
       process.env.NODE_ENV === "development" ? "AllWikis" : "AllWikisProd";
+    console.log(request);
     const result = await db.collection(collectionName).find().toArray();
     const response = NextResponse.json(result);
     response.headers.set("Cache-Control", "no-store, max-age=0");
